349b92ee62678bd3a6cd8ab97593e04e893393aa
mehranna@ca.ibm.com <mehr.najafi@gmail.com>
2017-10-19 20:11:15 -0400

    Initial Commit

---
+++ betterInvestorTests/betterInvestorTests.swift
@@ -0,0 +1,36 @@
+//
+//  betterInvestorTests.swift
+//  betterInvestorTests
+//
+//  Created by mehran najafi on 2017-10-19.
+//  Copyright © 2017 Ron. All rights reserved.
+//
+
+import XCTest
+@testable import betterInvestor
+
+class betterInvestorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
