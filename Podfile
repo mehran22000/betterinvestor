188fd787b4bc28e14136d6432d78942daf7c5816
mehran <mehran22000@yahoo.com>
2018-06-27 01:02:43 -0400

    Replaced project code with a similar one created for social trader

--- Podfile
+++ Podfile
@@ -1,23 +1,30 @@
 # Uncomment the next line to define a global platform for your project
-# platform :ios, '9.0'
+# platform :ios, '10.0'
 
-target 'betterInvestor' do
-    # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
-    use_frameworks!
-    
-    # Pods for betterInvestor
+target 'SocialTrader' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for SocialTrader
     pod 'Firebase’, ‘~>4.5.0’
     pod 'Firebase/AdMob'
     pod 'Charts'
     pod 'SideMenu'
-    target 'betterInvestorTests' do
-        inherit! :search_paths
-        # Pods for testing
-    end
-    
-    target 'betterInvestorUITests' do
-        inherit! :search_paths
-        # Pods for testing
-    end
-    
+    pod 'FBSDKCoreKit'
+    pod 'FBSDKLoginKit'
+    pod 'FBSDKShareKit'
+    pod 'SwiftyJSON', '~> 4.0'
+    pod 'Alamofire', '~> 4.7'
+ 
+
+  target 'SocialTraderTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'SocialTraderUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
 end
---
+++ SocialTrader/Constants.swift
@@ -0,0 +1,42 @@
+//
+//  Constants.swift
+//  betterInvestor
+//
+//  Created by mehran najafi on 2017-11-15.
+//  Copyright © 2017 Ron. All rights reserved.
+//
+
+import Foundation
+
+class Constants {
+    
+    // TODO: Enable multi-environments
+    // #if DEV
+    //    static let bsae_url = "https://betterinvestor-dev.herokuapp.com/services/v1/"
+    // #elseif PROD
+        static let bsae_url = "https://betterinvestor.herokuapp.com/services/v1/"
+    // #elseif ADHOC
+    //    static let bsae_url = "https://betterinvestor-staging.herokuapp.com/services/v1/"
+    // #else
+    //    static let bsae_url = "http://127.0.0.1:5000/services/v1/"
+    // #endif
+    
+    
+    // static let bsae_url = "https://betterinvestor-dev.herokuapp.com/services/v1/";
+    // static let bsae_url = "http://127.0.0.1:5000/services/v1/";
+
+    static let segmentControlHeight = 20;
+    static let segmentViewHeight = 30;
+    static let pageViewHeight = 190;
+    static let pageControlHeight = 30;
+    static let adViewHeight = 50;
+    static let portfolio_url = "user/portfolio/";
+    static let get_ranking_url = "user/portfolio/rankings/{mode}/{user_id}";
+    static let get_gains_url = "user/portfolio/gains/{user_id}";
+    static let get_quotes_url =  "market/stock/quote/array/{symbols}";
+    static let get_quote_url =  "market/stock/quote/{symbol}";
+    static let get_holders_url =  "users/holders/{symbol}/global/{isGlobal}/userid/{user_id}";
+    static let get_profile_url = "user/profile";
+    static let notif_stocks_updated = "quotes_updated";
+    static let status_success = "200";
+}
