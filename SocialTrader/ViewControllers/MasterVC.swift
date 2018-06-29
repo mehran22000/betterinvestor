2f57a62e037fb13fecf4759583c09f2d1760e08d
mehran <mehran22000@yahoo.com>
2018-06-29 17:01:54 -0400

    More iOS bug fixes.

--- SocialTrader/ViewControllers/MasterVC.swift
+++ SocialTrader/ViewControllers/MasterVC.swift
@@ -133,7 +133,7 @@ class MasterViewController: UIViewController, UITableViewDelegate, UITableViewDa
         self.yPortfolio = 0;
         self.ySegmentView = portfolioHeight;
         self.yPageView = self.ySegmentView! + Constants.segmentViewHeight;
-        self.yPageControlView = self.yPageView! + Constants.pageViewHeight - self.pageControlAdhustment();
+        self.yPageControlView = self.yPageView! + Constants.pageViewHeight - self.pageControlAdjustment();
         
         
         self.yAdView = yPageControlView! + Constants.pageControlHeight;
@@ -512,7 +512,7 @@ class MasterViewController: UIViewController, UITableViewDelegate, UITableViewDa
     // Page View Methods - End
     
     
-    func pageControlAdhustment() -> Int {
+    func pageControlAdjustment() -> Int {
         if UIDevice().userInterfaceIdiom == .phone {
             switch UIScreen.main.nativeBounds.height {
               case 1136: // iPhone 5 or 5S or 5C
@@ -520,7 +520,7 @@ class MasterViewController: UIViewController, UITableViewDelegate, UITableViewDa
               case 1334: // iPhone 6/6S/7/8
                 return 10;
               case 1920, 2208: // iPhone 6+/6S+/7+/8+
-                return 20;
+                return 15;
             case 2436: // iPhone X
                 return 55;
             default: // Unknown
